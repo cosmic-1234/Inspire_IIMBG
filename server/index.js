54fa388666238c060123a11132f1e42288e22b4d
cosmic-1234 <shrirangswanikar@gmail.com>
2026-01-14 18:10:03 +0530

    Fixed the error

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://inspire-client-tz7jmslgg-uc.a.run.app',
+        process.env.CLIENT_URL
+    ].filter(Boolean),
     credentials: true
 }));
 app.use(express.json());
